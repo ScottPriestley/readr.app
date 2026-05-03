c2ff2971e661af7d5badc948a07983b460f711f3
ScottPriestley <scott.priestley@gmail.com>
2026-05-03 13:46:12 -0400

    Changed topic news to Religion

--- src/App.jsx
+++ src/App.jsx
@@ -191,7 +191,7 @@ function ArticleCard({ article, onHide }) {
                article.topics?.[0] === 'space' ? '🚀' :
                article.topics?.[0] === 'health' ? '🏃' :
                article.topics?.[0] === 'food' ? '🍕' :
-               article.topics?.[0] === 'news' ? '📰' : '📄'}
+               article.topics?.[0] === 'religion' ? '📄' : }
             </span>
           </div>
         )}
